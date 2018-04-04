f05691ce00c04c64b81fa9b69419c3d0788e6973
Vinicius Borges <vbda123@gmail.com>
2018-04-04 18:06:18 -0300

    small changes on user output

--- problema-2/main.c
+++ problema-2/main.c
@@ -3,29 +3,37 @@
 int *recebe_notas(float *, int *, int);
 void *conta_nota(int *, int, int *);
 int percent_aprov(int *, float *);
+
 int main(int argc, char const *argv[]) {
+
   int num_elementos = 10;
   float notas[num_elementos];
   int apr[num_elementos], resultado[2] = {0,0};
   int i = 0, *p, *q, r;
   float percent_resultado[2] = {0,0};
 
+  printf("Insira as dez notas: \n");
+
   for(i = 0; i<num_elementos; i++){
     scanf("%f", &notas[i]);
   }
+
   p = recebe_notas(notas, apr, num_elementos);
   conta_nota(apr, num_elementos, resultado);
   r = percent_aprov(resultado, percent_resultado);
+
   printf("Quantidade de aprovados: %d\n", *(resultado));
   printf("Quantidade de reprovados: %d\n", *(resultado+1));
-  printf("Quantidade de aprovados: %.2f\n", *(percent_resultado));
-  printf("Quantidade de reprovados: %.2f\n", *(percent_resultado+1));
+  printf("Percentual de aprovados: %.2f\n", *(percent_resultado));
+  printf("Percentual de reprovados: %.2f\n", *(percent_resultado+1));
+
   if (r==1)
     printf("Mais da metade da turma aprovada\n");
   else
     printf("Metade ou mais da turma reprovada\n" );
   return 0;
 }
+
 int *recebe_notas(float *notas, int *apr, int num_elementos){
   int i = 0;
   for(i = 0; i<num_elementos; i++){
@@ -36,6 +44,7 @@ int *recebe_notas(float *notas, int *apr, int num_elementos){
   }
   return apr;
 }
+
 void *conta_nota(int *apr, int num_elementos, int *resultado){
   int i =0;
   for(i=0;i<num_elementos;i++){
@@ -45,6 +54,7 @@ void *conta_nota(int *apr, int num_elementos, int *resultado){
       *(resultado+1) += 1;
   }
 }
+
 int percent_aprov (int *resultado, float *percent_resultado){
   int i=0, num_total;
   *percent_resultado = (*resultado)*10;
